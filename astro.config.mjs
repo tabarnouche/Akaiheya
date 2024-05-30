f2e3ca2430789f14d978c23c16dd22d47161acec
Tetsuo Arakaki <arakaki@cerulean-net.jp>
2024-05-31 01:46:16 +1200

    👷 Github Pages CD設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tabarnouche.github.io',
+  base: 'Akaiheya',
   integrations: [alpinejs()]
 });
